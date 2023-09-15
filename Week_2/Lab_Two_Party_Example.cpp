a3880606d932fef2de7ebe44112616d32d4bbb0d
Cassandra <Cassandra.Laffan@gmail.com>
2023-09-15 12:56:01 -0400

    Adding in-lab examples

---
+++ Week_2/Lab_Two_Party_Example.cpp
@@ -0,0 +1,119 @@
+#include "Lab_Two_Party_Example.h"
+
+Party::Party(){
+    hostName.assign("");
+    location.assign("");
+    date.assign("");
+    startTime = -1.0;
+    endTime = -1.0;
+    maxGuests = -1;
+    currentGuests = 0;
+}
+
+Party::Party(std::string name, std::string loc, std::string day, float start, float end, int max = 50){
+    hostName.assign(name);
+    location.assign(loc);
+    date.assign(day);
+    startTime = start;
+    endTime = end;
+    maxGuests = max;
+    currentGuests = 0;
+    guestList = new std::string[maxGuests];
+}
+
+Party::~Party(){
+    delete guestList;
+}
+
+std::string Party::getHostName(){
+    return hostName;
+}
+
+void Party::setHostName(std::string name){
+    hostName.assign(name);
+}
+
+std::string Party::getPartyLocation(){
+    return location;
+}
+
+void Party::setPartyLocation(std::string loc){
+    location.assign(loc);
+}
+
+std::string Party::getPartyDate(){
+    return date;
+}
+
+void Party::setPartyDate(std::string day){
+    date.assign(day);
+}
+
+float Party::getStartTime(){
+    return startTime;
+}
+
+void Party::setStartTime(float time){
+    startTime = time;
+}
+
+float Party::getEndTime(){
+    return endTime;
+}
+
+void Party::setEndTime(float time){
+    endTime = time;
+}
+
+int Party::getMaxGuests(){
+    return maxGuests;
+}
+
+void Party::setMaxGuests(int max){
+    if(maxGuests == -1){
+        maxGuests = max;
+        guestList = new std::string[maxGuests];
+    }
+}
+
+int Party::getCurrentGuests(){
+    return currentGuests;
+}
+
+std::string* Party::getGuestList(){
+    return guestList;
+}
+
+bool Party::addGuestToGuestList(std::string name){
+    bool isAdded = false;
+
+    if(!(currentGuests == maxGuests -1)){
+        guestList[currentGuests].assign(name);
+        currentGuests++; // We want to be able to add at the next spot
+        isAdded = true;
+    }
+
+    return isAdded;
+}
+
+bool Party::deleteGuestFromList(std::string name){
+    bool isDeleted = false;
+
+    for(int i = 0; i <= currentGuests; i++){
+        if(guestList[i] == name && i != currentGuests){
+            for(int j = i; j < currentGuests; j++){
+                guestList[j].assign(guestList[j + 1]);
+            }
+            guestList[currentGuests].assign("");
+            currentGuests--;
+            isDeleted = true;
+        }
+        else if(guestList[i] == name && i == currentGuests){
+            guestList[i].assign("");
+            currentGuests--;
+            isDeleted = true;
+        }
+    }
+
+    return isDeleted;
+}
--- Week_2/Lab_Two_Party_Example.h
+++ Week_2/Lab_Two_Party_Example.h
@@ -1,4 +1,49 @@
+#ifndef PARTY_H
+#define PARTY_H
 /*
 * Here we are going to create a Party class! The party will have a date,
 * time, location, host and a dynamic guest list.
 */
+#include "iostream"
+
+class Party{
+    std::string hostName;
+    std::string location;
+    std::string date;
+    float startTime;
+    float endTime;
+    int maxGuests;
+    int currentGuests;
+    std::string* guestList;
+public:
+    Party();
+    Party(std::string, std::string, std::string, float, float, int);
+
+    ~Party(); // destructor
+
+    std::string getHostName();
+    void setHostName(std::string);
+
+    std::string getPartyLocation();
+    void setPartyLocation(std::string);
+
+    std::string getPartyDate();
+    void setPartyDate(std::string);
+    
+    float getStartTime();
+    void setStartTime(float);
+
+    float getEndTime();
+    void setEndTime(float);
+
+    int getMaxGuests();
+    void setMaxGuests(int); // should only be used once
+
+    int getCurrentGuests();
+
+    std::string* getGuestList();
+    bool addGuestToGuestList(std::string);
+    bool deleteGuestFromList(std::string);
+};
+
+#endif
