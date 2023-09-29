c7d3d2cdc3e2a9be67beb13d42c40b52a7f68708
Cassandra <Cassandra.Laffan@gmail.com>
2023-09-29 13:18:09 -0400

    Adding lab examples

--- Week_4/Bouquet_Lab_Example.cpp
+++ Week_4/Bouquet_Lab_Example.cpp
@@ -1,1 +1,75 @@
 #include "Bouquet_Lab_Example.h"
+
+// Safe state constructor
+Bouquet::Bouquet(){
+    this->bouqID = -1;
+    this->colourScheme.assign("");
+    this->flowerArraySize = -1;
+    this->currentFlower = -1;
+    this->flowerArray = nullptr;
+}
+
+Bouquet::Bouquet(int ID, std::string colour, int max){
+    this->bouqID = ID;
+    this->colourScheme.assign(colour);
+    this->flowerArraySize = max;
+    this->currentFlower = 0;
+    this->flowerArray = new Flower[max];
+}
+
+Bouquet::Bouquet(Bouquet& other){
+    // Consider how to check if other is initialized empty or not
+    this->bouqID = other.bouqID;
+    this->colourScheme.assign(other.colourScheme);
+    this->flowerArraySize = other.flowerArraySize;
+    this->flowerArray = new Flower[this->flowerArraySize];
+
+    for(int i = 0; i <= other.currentFlower; i++){
+        this->flowerArray[i] = Flower(other.flowerArray[i]);
+        this->currentFlower++;
+    }
+}
+
+Bouquet::~Bouquet(){
+    delete [] this->flowerArray;
+    this->flowerArray = nullptr; // not necessary but good practice
+}
+
+// Getters and setters
+
+void Bouquet::setMaxFlowers(int max){
+    if(!this->flowerArray){
+        this->flowerArraySize = max;
+        this->flowerArray = new Flower[max];
+    }
+}
+
+bool Bouquet::addFlowerToArray(Flower& aFlower){
+    bool succ = false;
+
+    if(this->currentFlower != this->flowerArraySize - 1){
+        this->flowerArray[this->currentFlower] = Flower(aFlower);
+        this->currentFlower++;  
+        succ = true;
+    }
+
+    return succ;
+}
+
+Bouquet& Bouquet::operator=(Bouquet& other){
+    if(this->flowerArraySize != -1){
+        this->~Bouquet();
+        //delete [] this->flowerArray;
+        //this->flowerArray = nullptr;
+    }
+    // REDUNDANT CODE TODO: MAKE BETTER (abstract into another function)
+    this->bouqID = other.bouqID;
+    this->colourScheme.assign(other.colourScheme);
+    this->flowerArraySize = other.flowerArraySize;
+    this->flowerArray = new Flower[this->flowerArraySize];
+
+    for(int i = 0; i <= other.currentFlower; i++){
+        this->flowerArray[i] = Flower(other.flowerArray[i]);
+        this->currentFlower++;
+    }
+}
--- Week_4/Bouquet_Lab_Example.h
+++ Week_4/Bouquet_Lab_Example.h
@@ -12,11 +12,36 @@
 
 #include "Current_Object_Example.h"
 
-
 class Bouquet{
-
+    int bouqID;
+    std::string colourScheme;
+    int flowerArraySize; // necessary for assigning memory
+    int currentFlower;
+    Flower* flowerArray;
 public:
+    Bouquet();
+    Bouquet(int, std::string, int);
+    Bouquet(Bouquet&); // reference to another Bouquet object
 
+    ~Bouquet(); // necessary because of dynamic memory
+
+    // Getters (constant today)
+
+    int getBouqID() const;
+    std::string getColourScheme() const;
+    int getMaxFlowers() const;
+    Flower* getFlowerArray() const;
+
+    // Setters
+
+    void setBouqID(int);
+    void setColourScheme(std::string);
+    void setMaxFlowers(int);
+    // returns a bool of whether or not this is successful
+    bool addFlowerToArray(Flower&);
+    
+    // Operator overloading
+    Bouquet& operator=(Bouquet&);
 };
 
 #endif
