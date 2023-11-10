cdbd07c2926ee5c303c0c33ef5cd409f03463ab1
Cassandra <Cassandra.Laffan@gmail.com>
2023-11-10 13:09:16 -0500

    Adding lab examples

--- Week_9/Circle.cpp
+++ Week_9/Circle.cpp
@@ -1,1 +1,25 @@
 #include "Circle.h"
+
+Circle::Circle(){
+    radius = 0;
+}
+
+Circle::Circle(float r){
+    radius = r;
+}
+
+float Circle::getRadius() const{
+    return radius;
+}
+
+void Circle::setRadius(float r){
+    radius = r;
+}
+
+float Circle::parameter() const{
+    return 2 * M_PI * radius; // TODO: Figure out why math pi isn't working
+}
+
+float Circle::area() const{
+    return (M_PI * pow(radius, 2));
+}
--- Week_9/main.cpp
+++ Week_9/main.cpp
@@ -1,4 +1,5 @@
 #include "Square.h"
+#include "Circle.h"
 
 int main(){
     // Try uncommenting this line! You won't be able to compile
@@ -13,8 +14,15 @@ int main(){
     // Cursed
     Shape* maybeAShape = nullptr;
 
+    Circle D;
+
+    Circle E(2.1);
+
     std::cout << "The parameter for square B is: " << B.parameter() << std::endl;
     std::cout << "The parameter for square C is: " << C.parameter() << std::endl;
+
+    std::cout << "The parameter for circle D is: " << D.parameter() << std::endl;
+    std::cout << "The parameter for circle E is: " << E.parameter() << std::endl;
 
     return 0;
 }
