08f967b41b8882ae43dce67adb638001088f54d6
cassLaffan <poisonberryvines@gmail.com>
2024-03-06 13:26:00 -0500

    Adding tomorrow's exercise

---
+++ Week_8/Animal.h
@@ -0,0 +1,16 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+/* Today, we will work on a few concepts. We will make an Animal base
+ * class with a few virtual functions. Then, we will make a cat class
+ * which has a friend function that displays its data.
+ * 
+ * Animal won't be a pure abstract base class, we will go over that
+ * next week.
+ */
+
+class Animal{};
+
+class Cat : public Animal{};
+
+#endif
