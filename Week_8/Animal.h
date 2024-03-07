7f379de19537ea9c83194527a3c3b77f2d1abd1a
Cassandra <Cassandra.Laffan@gmail.com>
2024-03-07 12:36:28 -0500

    Adding this week's examples for you to play around with

--- Week_8/Animal.h
+++ Week_8/Animal.h
@@ -1,6 +1,9 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
+#include <iostream>
+#include <cstring>
+
 /* Today, we will work on a few concepts. We will make an Animal base
  * class with a few virtual functions. Then, we will make a cat class
  * which has a friend function that displays its data.
@@ -9,8 +12,36 @@
  * next week.
  */
 
-class Animal{};
+class Animal{
+    int animalID;
+    std::string colour;
+public:
+    Animal();
+    Animal(int, std::string);
+    Animal(Animal&);
 
-class Cat : public Animal{};
+    int getID() const;
+    void setID(int);
+
+    std::string getColour() const;
+    void setColour(std::string);
+
+    virtual std::ostream& display(std::ostream&);
+};
+
+class Cat : public Animal{
+    std::string breed;
+public:
+    Cat();
+    Cat(std::string, int, std::string);
+    Cat(Cat&);
+
+    std::string getBreed() const;
+    void setBreed(std::string);
+
+    std::ostream& display(std::ostream&);
+
+    friend void makeMeow(Cat, int); // Make the cat meow this many times
+};
 
 #endif
