10750b7914678c3fc1656902f93d7de7287c2eab
Cassandra <Cassandra.Laffan@gmail.com>
2024-03-14 09:52:59 -0400

    Adding actual polymorphic behaviour

--- Week_9/main.cpp
+++ Week_9/main.cpp
@@ -7,6 +7,8 @@ int main(){
 
     Square B;
     Square C(6.3, 5.3);
+    Circle D;
+    Circle E(2.1);
 
     // A chameleon of an object!
     Square* maybeASquare = nullptr;
@@ -14,9 +16,15 @@ int main(){
     // Cursed
     Shape* maybeAShape = nullptr;
 
-    Circle D;
+    maybeAShape = new Square(12, 3);
+    std::cout << "The area for the shape that is a square at runtime is: " 
+        << maybeAShape->area() << std::endl;
 
-    Circle E(2.1);
+    delete maybeAShape;
+
+    maybeAShape = new Circle(3.2);
+    std::cout << "The area for the shape that is now a circle at runtime is: " 
+        << maybeAShape->area() << std::endl;
 
     std::cout << "The parameter for square B is: " << B.parameter() << std::endl;
     std::cout << "The parameter for square C is: " << C.parameter() << std::endl;
