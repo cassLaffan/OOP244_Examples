d81f44a281c42c93c00dda4f819bce308aa5936d
cassLaffan <poisonberryvines@gmail.com>
2024-03-21 10:29:36 -0400

    Fleshing out the override example a bit

--- Week_11/Override.cpp
+++ Week_11/Override.cpp
@@ -5,11 +5,23 @@
 
 class Base {
 public:
-    virtual void display() const;
+    virtual void print() const {
+        std::cout << "Base::print()" << std::endl;
+    }
 };
 
 class Derived : public Base {
 public:
-    void display() const override;
+    // Correct usage with the override keyword
+    void print() const override {
+        std::cout << "Derived::print()" << std::endl;
+    }
 };
+
+int main() {
+    Derived d;
+    Base& b = d;
+    b.print(); // This will call Derived::print()
+    return 0;
+}
 
