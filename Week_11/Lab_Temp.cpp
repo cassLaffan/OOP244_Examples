ce3fb6a9b068d1dc5e5ace63fa1b822222fd5fd3
cassLaffan <poisonberryvines@gmail.com>
2024-03-28 21:48:03 -0400

    Forgot to commit today's changes

--- Week_11/Lab_Temp.cpp
+++ Week_11/Lab_Temp.cpp
@@ -1,1 +1,51 @@
 #include "Lab_Temp.h"
+
+int main(){
+    Person<int> personA;
+    personA.setName(18);
+
+    Person<std::string> personB;
+    personB.setName("Cass");
+
+    std::cout << "Number name: " << personA.getName() << std::endl;
+    std::cout << "Actual name: " << personB.getName() << std::endl;
+
+    aPrintFunction("Cass", 1994);
+
+    aPrintFunction(3.333, 3.333);
+
+    float exA = 3.14;
+    int exB = 1;
+
+    swap(exA, exB);
+    
+    std::cout << "Swapped values are: " << exA << " and " << exB << std::endl;
+
+    std::string exC = "Cheetos";
+    /*
+    int exD = 2;
+
+    swap(exC, exD);
+
+    std::cout << "Swapped values are: " << exC << " and " << exD << std::endl;
+    */
+
+    char exE = 'a';
+    float exF = 9.888;
+
+    swap(exE, exF);
+
+    std::cout << "Swapped values are: " << exE << " and " << exF << std::endl;
+
+    // Undefined behaviour
+    // swap(exC, exE);
+
+    // Why
+    int a = -0;
+
+    std::cout << a << std::endl;
+    std::cout << (bool)a << std::endl;
+
+
+    return 0;
+}
--- Week_11/Lab_Temp.h
+++ Week_11/Lab_Temp.h
@@ -1,9 +1,39 @@
 #ifndef LAB_H
 #define LAB_H
 
-// Today, let's playn around with templates. First, let's write a template
+#include <iostream>
+
+// Today, let's play around with templates. First, let's write a template
 // class which can make a "dynamic" array at runtime. Let's see if it works!
 // Then, let's write a few template functions which play around with multiple
 // types and brainstorm ways to use them.
+
+template<class T>
+class Person{
+    T name;
+public:
+    T getName() const{
+        return name;
+    }
+
+    void setName(T aName){
+        name = aName;
+    }
+
+};
+
+template <typename T, typename E>
+void aPrintFunction(T argA, E argB){
+    std::cout << "Our first argument is: " << argA << std::endl;
+    std::cout << "Our second argument is: " << argB << std::endl;
+}
+
+template<typename T, typename E>
+void swap(T& a, E& b) {
+    T c;
+    c = a;
+    a = b;
+    b = c;
+}
 
 #endif
