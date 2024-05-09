85d13d09f64c8c3e1bab6c6eca4da14465297aa6
cassLaffan <poisonberryvines@gmail.com>
2024-05-09 08:39:15 -0400

    Ensuring the files are empty for the demonstration tomorrow

--- Week_1/Lab_One_Book_Example.h
+++ Week_1/Lab_One_Book_Example.h
@@ -1,8 +1,3 @@
-#ifndef BOOK_H
-#define BOOK_H
-
-#include <iostream>
-
 /*
 Today, we will program a basic class and a few "getters"
 and "setters". What are those? They are the basic "member"
@@ -10,20 +5,3 @@ functions for object oreiented programming. They "get"
 member attributes so we don't have to access the innards of
 an object directly, and "set" said members!
 */
-
-class Book{
-    int bookID;
-    std::string bookTitle;
-public:
-    Book(int, std::string); // constructor
-
-    // Getters
-    int getBookID() const; // safe constant!
-    std::string getBookTitle() const;
-
-    // Setters
-    void setBookID(int);
-    void setBookTitle(std::string);
-};
-
-#endif
