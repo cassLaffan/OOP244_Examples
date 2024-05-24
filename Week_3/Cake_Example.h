d3df73f11fe362c11e1168afd7c971a2c5e90cd7
cassLaffan <poisonberryvines@gmail.com>
2024-05-24 11:09:12 -0400

    Cleaning up for the lab today

--- Week_3/Cake_Example.h
+++ Week_3/Cake_Example.h
@@ -1,5 +1,6 @@
 #ifndef CAKE_H
 #define CAKE_H
+
 /*
 * In this lab, we are going to create a cake class. It will have
 * the following member variables: flavour, colour, tiers, weight
@@ -8,44 +9,6 @@
 * We are also going to write its getters, setters, constructors. Finally,
 * we are going to overload the following operators: ==, >=, <=, and =
 */
-
-#include "iostream"
-
-class Cake{
-    std::string flavour;
-    std::string colour;
-    int tiers;
-    float weight;
-    std::string bakerName;
-public:
-    Cake();
-    Cake(std::string, std::string, int, float, std::string);
-
-    std::string getFlavour() const;
-    void setFlavour(std::string);
-
-    std::string getColour() const;
-    void setColour(std::string);
-
-    int getTiers() const;
-    void setTiers(int);
-
-    float getWeight() const;
-    void setWeight(float);
-
-    std::string getBaker() const;
-    void setBaker(std::string);
-
-    // Operators: ==, >=, <=, and =
-
-    bool operator==(Cake&) const;
-    bool operator==(float&) const;
-
-    bool operator>=(Cake&) const;
-    bool operator<=(Cake&) const;
-
-    Cake& operator=(Cake&);
-};
 
 
 
