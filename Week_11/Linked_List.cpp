8cd0a121f3dfe730257ab6a5f5d0753798c7b332
cassLaffan <poisonberryvines@gmail.com>
2024-07-26 09:44:33 -0400

    Adding this week's files

---
+++ Week_11/Linked_List.cpp
@@ -0,0 +1,12 @@
+#include "Linked_List.h"
+
+/*
+* Here, we will be actually creating a linked
+* list out of node class objects. Creating Nodes
+* which point to the next object naturally give
+* rise to the linked list data structure!
+*/
+
+int main(){
+    return 0;
+}
---
+++ Week_11/Linked_List.h
@@ -0,0 +1,21 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+/*
+* Today, we'll be creating a data structure
+* called a "linked list" using C++ class templates.
+* A linked list is, in essence, a data structure
+* (a class) which has two data members. Whatever its
+* "data" is (int, Student, float, whatever!) and a pointer
+* to the next "node" in said linked list.
+
+* To accomplish this, we will be creating a "Node"
+* template class.
+*/
+#include <iostream>
+
+template <class T>
+class Node {
+    // Logic goes here
+};
+
+#endif
